9ed0278976156f3c4abc1e8b3467c86f48cef178
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-11 20:08:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
 
   name_hash.each do |item, value|
-    if lowest_value == nil or value < lowest_value 
+    if lowest_value == nil or value < lowest_value
       lowest_value = value
       lowest_item = item
     end
